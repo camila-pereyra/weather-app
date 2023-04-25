26231a09241f3bab74402129282c61a3339266b4
camila.pereyra <campereyra23051995@gmail.com>
2023-04-25 16:56:05 -0300

    Ajuste en estilos de Card

--- src/components/ContainerCards/ContainerCards.jsx
+++ src/components/ContainerCards/ContainerCards.jsx
@@ -4,12 +4,12 @@ import "./ContainerCards.css"
 const ContainerCards = ({list}) => {
    
   return (
-    <div>
+    <>
       <h2 className="containerName">Forecast Weather</h2>
       <div className="containerCards">
         {list!==undefined && list.map((element,index)=><Card dt={element.dt} temp={element.main.temp} icon={element.weather[0].icon} key={index} main={element.weather[0].main}/>) }
       </div>
-    </div>
+    </>
     )
   }
 
